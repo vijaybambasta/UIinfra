4ec4a3d64d7e1524446df3b0de955beeafe112c8
vijaybambasta <99092446+vijaybambasta@users.noreply.github.com>
2022-02-09 14:15:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "domain_name" {
   type        = string
   description = "The domain name for the website."
-  default = "FROMUIVIJAY78"
+  default = "fromvijay78"
 }
 
 variable "bucket_name" {
   type        = string
   description = "The name of the bucket without the www. prefix. Normally domain_name."
-  default = "FROMVIJAY81"
+  default = "fromvijay81"
 }
 
 variable "common_tags" {

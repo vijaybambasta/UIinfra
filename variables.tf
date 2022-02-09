d19a3282a2087b4e0669b158d72771482e2a3094
vijaybambasta <99092446+vijaybambasta@users.noreply.github.com>
2022-02-09 13:25:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "domain_name" {
   type        = string
   description = "The domain name for the website."
+  default = "FROMUIVIJAY78"
 }
 
 variable "bucket_name" {
   type        = string
   description = "The name of the bucket without the www. prefix. Normally domain_name."
+  default = "FROMVIJAY81"
 }
 
 variable "common_tags" {
   description = "Common tags you want applied to all components."
+  default = "VIJAY"
 }
